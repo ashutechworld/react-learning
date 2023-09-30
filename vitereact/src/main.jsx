7da6d79e539d9be0d1bcd22af67533f1b3baff02
Ashutosh singh <sashu9853@gmail.com>
2023-10-01 00:36:09 +0530

    add all files

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM  from 'react-dom/client';
 import App from './App.jsx'
 
+
+
+const createElement = React.createElement(
+  "a",
+  {href: "https://www.google.com/", target: "_blank"},
+  "click me to visit rgere"
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      createElement
 )
